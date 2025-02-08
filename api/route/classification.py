c5b5982bec03c6d5d215ad48890f85d49459993a
Mahasvan <mahasvan@gmail.com>
2025-02-08 16:11:24 +0530

    make it all post requests smh

--- api/route/classification.py
+++ api/route/classification.py
@@ -1,6 +1,7 @@
 import json
 
 from fastapi import APIRouter
+from pydantic import BaseModel
 from starlette.responses import JSONResponse
 
 from api.service import chat_handler
@@ -8,28 +9,31 @@ from api.service import chat_handler
 router = APIRouter()
 prefix = "/classification"
 
+class Input(BaseModel):
+    description: str
+
 industries = ["Biofuels",
               "Biotechnology & Pharmaceuticals",
               "Software & IT Services",
               "Food Retailers & Distributors",
               "Oil & Gas – Exploration & Production"]
 
-@router.get("/industry")
-async def classify_industry(description):
+@router.post("/industry")
+async def classify_industry(description: Input):
     """Identify a company's industry based on its mission statement and description."""
     response = chat_handler.industry_classification_chain.invoke(input={
-        "description": description,
+        "description": description.description,
     })
     industry = industries[int(response['text'])]
     return {
         "industry": industry,
     }
 
-@router.get("/initiative")
-async def classify_csr_initiative(description):
+@router.post("/initiative")
+async def classify_csr_initiative(description: Input):
     """extracts information from a CSR Initiative's writeup"""
     response = chat_handler.initiative_classification_chain.invoke(input={
-        "description": description,
+        "description": description.description,
     })
     return JSONResponse({
         "response": json.loads(response['text'])
